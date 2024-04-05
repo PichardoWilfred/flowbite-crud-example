13e0692e0c31f2740301050c1654ce3c3f9e2f8e
PichardoWilfred <pichardowilfred@gmail.com>
2024-04-05 01:04:36 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// const form =  document.querySelector("form#add-student");
+// const modal_btn = document.querySelector("button#add-student");
+
+// modal_btn.addEventListener('click', () => {
+    
+// });
